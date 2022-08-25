1ec233b04c12fee247533d53b6fa73fef7ac4075
Ezra-code <njerukithinji872@gmail.com>
2022-08-25 21:47:04 +0300

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+      create_table :students do |t|
+      t.string :name
+      # t.string :genre
+      # t.integer :age
+      # t.string :hometown
+      # the id column is gemerated automatically for every table! no need to specify it here.
+    end
   end
 end
